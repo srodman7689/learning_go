12abd3068887e14e23449e5f5c53c7b18860d067
Sean Rodman <srodman7689@gmail.com>
2016-09-15 20:21:57 -0500

    Finished the view_layer module. About to start part 1 of the controller module.

---
+++ creating_web_applications_in_go/view_layer/viewmodels/home.go
@@ -0,0 +1,15 @@
+package viewmodels
+
+type Home struct {
+	Title  string
+	Active string
+}
+
+func GetHome() Home {
+	result := Home{
+		Title:  "Lemonade Stand Society",
+		Active: "home",
+	}
+
+	return result
+}
